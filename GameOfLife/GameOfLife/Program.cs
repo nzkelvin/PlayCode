1cd193e8814e891379b67221ac50e4c313cff1a9
Kelvin Shen <kelvinshen@outlook.com>
2015-06-29 08:24:50 +1200

    The core logic is working.

---
+++ GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // [Length(row number), Width(column number)]
+            int[,] initialWorld = new int[4, 3] {{0,1,0},
+                                                 {0,1,0},
+                                                 {0,1,0},
+                                                 {0,0,0}};
+
+            var world = new World(initialWorld);
+
+            for (var i = 0; i < 5; i++)
+            {
+                world.Evolve();
+
+                var tmp = world.ToString();
+            }
+
+            Console.Read();
+        }
+    }
+
+    public class World
+    {
+        public int Length { get; set; } // Y direction
+        public int Width { get; set; } // X direction
+        public Cell[,] CurrentGen { get; private set; }
+        private Cell[,] _nextGen;
+
+        public World(int[,] initWorld)
+        {
+            Length = initWorld.GetLength(0);
+            Width = initWorld.GetLength(1);
+
+            CurrentGen = new Cell[Length, Width]; 
+            _nextGen = new Cell[Length, Width];
+
+            for(var r = 0; r < Length; r++)
+            {
+                for(var c = 0; c < Width; c++)
+                {
+                    CurrentGen[r, c] = new Cell() { Row = r, Column = c, Status = (CellStatus)initWorld[r, c] };
+                }
+            }
+
+            for (var r = 0; r < Length; r++)
+            {
+                for (var c = 0; c < Width; c++)
+                {
+                    _nextGen[r, c] = new Cell() { Row = r, Column = c, Status = CellStatus.Dead };
+                }
+            }
+        }
+
+        public void Evolve()
+        {
+            for (var r = 0; r < Length; r++)
+            {
+                for (var c = 0; c < Width; c++)
+                {
+                    _nextGen[r, c].UpdateStatus(CurrentGen);
+                }
+            }
+
+            // Once evaluation completes, next Generation becomes this evaluation
+            for (var r = 0; r < Length; r++)
+            {
+                for (var c = 0; c < Width; c++)
+                {
+                    CurrentGen[r, c].Status = _nextGen[r, c].Status;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            var textBuilder = new StringBuilder();
+
+            for (var r = 0; r < Length; r++)
+            {
+                for (var c = 0; c < Width; c++)
+                {
+                    textBuilder.Append((int)CurrentGen[r, c].Status);
+                }
+
+                textBuilder.AppendLine();
+            }
+
+            return textBuilder.ToString();
+        }
+    }
+
+    public class Cell
+    {
+        public int Column { get; set; }
+        public int Row { get; set; }
+        public CellStatus Status { get; set; }
+
+        public void UpdateStatus(Cell[,] currentWorld)
+        {
+            int liveNeighbourCount = 0;
+
+            for (var r = Row - 1; r <= Row + 1; r++)
+            {
+                for (var c = Column - 1; c <= Column + 1; c++)
+                {
+                    if (r >= 0 && r < currentWorld.GetLength(0) 
+                        && c >= 0 && c < currentWorld.GetLength(1) 
+                        && currentWorld[r, c].Status == CellStatus.Alive)
+                    {
+                        if (r == Row && c == Column)
+                            continue;
+
+                        liveNeighbourCount++;
+                    }
+                }
+            }
+
+            //if (liveNeighbourCount < 2 || liveNeighbourCount > 3)
+            //{
+            //    Status = CellStatus.Dead;
+            //}
+
+            if (liveNeighbourCount == 3)
+            {
+                Status = CellStatus.Alive;
+            }
+            else if (liveNeighbourCount == 2)
+            {
+                Status = currentWorld[Row, Column].Status;
+            }
+            else
+            {
+                Status = CellStatus.Dead;
+            }
+
+
+
+            // if 2, nothing changes. Lived keeps alive
+        }
+    }
+
+    public enum CellStatus
+    {
+        Dead=0,
+        Alive=1
+    }
+}
---
+++ GameOfLife/GameOfLife/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GameOfLife")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GameOfLife")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ca674e6a-ce1d-46d1-b950-7ab314a67063")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
