da5bb8f302eb071123962d3075e2e2037cb1dc9f
Kelvin Shen <kelvinshen@outlook.com>
2015-07-01 07:35:08 +1200

    Windows Store App experiment.

---
+++ SharingExample/OtherSharingExamples/LinkedFile/HelloWorldClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shared
+{
+    public class HelloWorldClass
+    {
+#if WINDOWS_APP
+        private string _platform = "Windows Store";
+#elif WINDOWS_PHONE_APP
+        private string _platform = "Windows Phone";
+#else
+        private string _platform = "...";
+#endif
+
+        public string SayHi()
+	    {
+            return "Hello from " + _platform;
+	    }
+    }
+}
