6da1cfc5a78b1154f76f0120ba5ec0947977b3cb
Kelvin Shen <kelvinshen@outlook.com>
2015-07-02 07:17:13 +1200

    PortableClassLibrary

---
+++ SharingExample/OtherSharingExamples/PortableClassLibrary/HelloWorldClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortableClassLibrary
+{
+    public class HelloWorldClass
+    {
+        private string _platform;
+
+        public HelloWorldClass(string platform)
+        {
+            _platform = platform;
+        }
+
+        public string SayHi()
+        {
+            return "Hello from " + _platform;
+        }
+    }
+}
--- SharingExample/OtherSharingExamples/PortableClassLibrary/YellowPage.xaml.cs
+++ SharingExample/OtherSharingExamples/PortableClassLibrary/YellowPage.xaml.cs
@@ -15,24 +15,21 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace WindowsStore
+namespace PortableClassLibrary
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class YellowPage : Page
     {
-        public MainPage()
+        public YellowPage()
         {
             this.InitializeComponent();
-
-            MyTextBlock.Text = new Shared.HelloWorldClass().SayHi();
-            MyTextBlock.Text = new PortableClassLibrary.HelloWorldClass("Windows Store App").SayHi();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(PortableClassLibrary.YellowPage));
+
         }
     }
 }
