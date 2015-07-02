ec526c14110e89197f03547d52e5cc47a3a2aaf2
Kelvin Shen <kelvinshen@outlook.com>
2015-07-03 06:22:27 +1200

    JavaScriptPhone project

---
+++ SharingExample/JavaScriptPhone/js/default.js
@@ -0,0 +1,28 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=329104
+(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+
+            document.getElementById("MyTextBlock").innerText = WinRTComponent.HelloWorldClass("JS phone").sayHi();
+
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
+})();
---
+++ SharingExample/OtherSharingExamples/WinRTComponent/HelloWorldClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinRTComponent
+{
+    /*
+     * WinRT has quite a few restrictions. In terms of, what you can or cannot do. 
+     * So use this when you have to. However WinRT component can be shared to differnt languages.
+     */
+    public sealed class HelloWorldClass
+    {
+        private string _platform;
+
+        public HelloWorldClass(string platform)
+        {
+            _platform = platform;
+        }
+
+        public string SayHi()
+        {
+            return "Hello from " + _platform;
+        }
+    }
+}
