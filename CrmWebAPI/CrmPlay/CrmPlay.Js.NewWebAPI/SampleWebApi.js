00aee3d1774456209e9a29aff704ff896e40c72a
Kelvin Shen <kelvinshen@outlook.com>
2015-07-06 06:11:00 +1200

    CrmPlay.JS.NewAPI

---
+++ CrmWebAPI/CrmPlay/CrmPlay.Js.NewWebAPI/SampleWebApi.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="Sdk.WebAPIPreview.js" />
+
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        startSample();
+    }
+}
+
+//Simple error handler used by all samples just writes message to console
+function sampleErrorHandler(error) {
+    console.log(error.message);
+}
+
+function startSample() {
+    addContact();
+}
+
+function addContact() {
+    console.log("addContact function starting.");
+    var contactAUri;
+
+    var contactA = {
+        firstname: "Tom",
+        lastname: "Test"
+    };
+
+    Sdk.WebAPIPreview.create("contacts", contactA, function (url) {
+        console.log("a contact was created. url: " + url);
+    }, sampleErrorHandler);
+}
