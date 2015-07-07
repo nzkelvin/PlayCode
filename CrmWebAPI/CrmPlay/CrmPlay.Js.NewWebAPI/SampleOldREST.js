418829c85ec42f32d66af61511dbee2048a8d3ac
Kelvin Shen <kelvinshen@outlook.com>
2015-07-07 15:39:25 +1200

    js sample of using existing REST API

---
+++ CrmWebAPI/CrmPlay/CrmPlay.Js.NewWebAPI/SampleOldREST.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="SDK.REST.js" />
+
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        startSample();
+    }
+}
+
+//Simple error handler used by all samples just writes message to console
+function sampleErrorHandler(error) {
+    console.log(error.message);
+}
+
+function startSample() {
+    addContact();
+}
+
+function addContact() {
+    console.log("addContact function starting.");
+    var contactAUri;
+
+    var contact = {
+        firstname: "Tom",
+        lastname: "Test from Old REST API"
+    };
+
+    SDK.REST.createRecord(
+        contact,
+        "Contact",
+        function (contact) {
+            console.log("An contact is created. Id: " + contact.ContactId);
+        }
+    );
+}
