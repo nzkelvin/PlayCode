242d105463dd7af90862bb7eb30012267f015401
Kelvin Shen <kelvinshen@outlook.com>
2015-07-19 21:38:14 +1200

    Simple async code play

---
+++ CSharpPlay/CSharpPlay.Async/AuthenticationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPlay.Async
+{
+    public class AuthenticationService
+    {
+        private readonly SimpleDataContext data;
+
+        public AuthenticationService()
+        {
+            data = SimpleDataContext.DemoData;
+        }
+
+        public async Task<Guid?> AuthenticateUserAsync(string username, string password)
+        {
+            await Task.Delay(5000);
+
+            var id = data.Users.Where(u => u.UserName == username && u.Password == password)
+                               .Select(u => (Guid?)u.Id)
+                               .SingleOrDefault();
+
+            return id;
+        }
+    }
+}
---
+++ CSharpPlay/CSharpPlay.Async/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPlay.Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+
+            Console.Read();
+        }
+
+        static async void Run()
+        {
+            AuthenticationService service = new AuthenticationService();
+
+            var id = await service.AuthenticateUserAsync("jon", "c#5");
+
+            Console.WriteLine(id);
+        }
+    }
+}
