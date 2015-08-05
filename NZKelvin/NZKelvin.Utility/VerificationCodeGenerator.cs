9f429664e82d403d8f48df029ab7ce65a25aeca4
Kelvin Shen <kelvinshen@outlook.com>
2015-08-05 16:03:19 +1200

    Verification code generation

---
+++ NZKelvin/NZKelvin.Utility/VerificationCodeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZKelvin.Utility
+{
+    public class VerificationCodeGenerator
+    {
+        public string Generate()
+        {
+            var timeBytes = BitConverter.GetBytes(DateTime.UtcNow.ToBinary()); // BitConvert coverts primary type to byte[]
+            var guidBytes = Guid.NewGuid().ToByteArray(); // Guid is a reference type. It has its own convert method.
+            return Convert.ToBase64String(guidBytes.Concat(timeBytes).ToArray());
+        }
+
+        /// <summary>
+        /// Use case: You want to expire the verification code in 24 hours. You can get time from the verification code.
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public DateTime? GetGeneratedTime(string code)
+        {
+            var timeBytes = Convert.FromBase64String(code).ToArray();
+            var timeBinary = BitConverter.ToInt64(timeBytes, 16);
+            return DateTime.FromBinary(timeBinary);
+        }
+    }
+}
---
+++ NZKelvin/Tests/NZKelvin.Utility.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NZKelvin.Utility;
+
+namespace NZKelvin.Utility.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var gen = new VerificationCodeGenerator();
+            var code = gen.Generate();
+            
+            
+
+
+            var time = gen.GetGeneratedTime(code);
+        }
+    }
+}
