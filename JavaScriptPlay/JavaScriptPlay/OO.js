73d01f9afc01a8c4ef6e623eb728ff1cfce77167
Kelvin Shen <kelvinshen@outlook.com>
2015-09-02 07:24:20 +1200

    js play

---
+++ JavaScriptPlay/JavaScriptPlay/OO.js
@@ -0,0 +1,25 @@
+﻿//Constructor
+function Pager() {
+    this.CurrentPage = 0;
+}
+
+Pager.maxRecordsPerPage = 4;
+
+//This is for object
+Pager.prototype.nextPage = function () {
+    this.CurrentPage++;
+    console.log("from prototype");
+};
+
+////This is static
+//Pager.nextPage = function () {
+//    console.log("from static object");
+//};
+
+// This function will execute first.
+(function main() {
+    var mypager = new Pager();
+    mypager.nextPage();
+    console.log(mypager.CurrentPage);
+    console.log(Pager.maxRecordsPerPage);
+})();
