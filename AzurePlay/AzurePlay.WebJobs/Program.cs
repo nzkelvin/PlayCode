0ded833917962e2b3dbd075b908454d1a1ec41a3
Kelvin Shen <kelvinshen@outlook.com>
2015-09-04 16:44:55 +1200

    Proj initial setup

---
+++ AzurePlay/AzurePlay.WebJobs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzurePlay.WebJobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("Test");
+                Thread.Sleep(5000);
+                i++;
+            }
+        }
+    }
+}
