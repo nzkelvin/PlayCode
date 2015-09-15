e9a303449912bcba1f3664cffdb0b171c09f0a3b
Kelvin Shen <kelvinshen@outlook.com>
2015-09-16 05:22:52 +1200

    js namespace

---
+++ JavaScriptPlay/JavaScriptPlay/NameSpace.js
@@ -0,0 +1,18 @@
+﻿//if (typeof (KelvinS) == typeof (undefined))
+//    KelvinS = {};
+
+//if (typeof (KelvinS.Crm) == typeof (undefined))
+//    KelvinS.Crm = {
+//        getGuid: function() { alert("Hello Guid") }
+//    };
+
+
+var KelvinS = KelvinS || {};
+KelvinS.Crm = KelvinS.Crm || {
+    getGuid: function () { alert("Hello Guid") }
+};
+
+// Run code here
+(function Main() {
+    KelvinS.Crm.getGuid();
+})();
