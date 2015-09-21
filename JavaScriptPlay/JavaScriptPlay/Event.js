6c38319fe6bafa43378757242e12f5a56dce2caf
Kelvin Shen <kelvinshen@outlook.com>
2015-09-22 07:42:21 +1200

    js event

---
+++ JavaScriptPlay/JavaScriptPlay/Event.js
@@ -0,0 +1,25 @@
+﻿var JavaScriptPlay = JavaScriptPlay || {};
+
+var Item = function (name, id) {
+    this.name = name;
+    this.id = id;
+    this.onExecutionCompleteHandlers = [];
+
+};
+
+Item.prototype.addExecutionCompleteHandler = function (handler) {
+    this.onExecutionCompleteHandlers.push(handler);
+};
+
+Item.prototype.Execution = function () {
+    for (i = 0; i < this.onExecutionCompleteHandlers.length; i++) {
+        this.onExecutionCompleteHandlers[i]();
+    }
+};
+
+(function main() {
+    var myItem = new Item("box", 1);
+    myItem.addExecutionCompleteHandler(function () { alert(myItem.id) });
+
+    myItem.Execution();
+})();
