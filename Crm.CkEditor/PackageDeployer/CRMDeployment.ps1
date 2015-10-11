e7ea653a2eee7b34c0b34ff42577901ad94dfd09
Kelvin Shen <kelvinshen@outlook.com>
2015-10-12 07:29:19 +1300

    CRM deployment packages and script.

---
+++ Crm.CkEditor/PackageDeployer/CRMDeployment.ps1
@@ -0,0 +1,6 @@
+﻿$PackageDirectory = "C:\Users\kelvi_000\Documents\Crm 2015\SDK\Tools\PackageDeployer"
+cd $PackageDirectory
+Add-PSSnapin Microsoft.Xrm.Tooling.Connector
+Add-PSSnapin Microsoft.Xrm.Tooling.PackageDeployment
+$CRMConn = Get-CrmConnection -DeploymentRegion Oceania -OnlineType Office365 -OrganizationName Augendev -Credential $Cred
+Import-CrmPackage -CrmConnection $CRMConn -PackageDirectory $PackageDirectory -PackageName crmpackagedeployment.dll -Verbose
