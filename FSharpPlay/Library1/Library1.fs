d2006ab5fdc41e809608812028814c1ad8174d6d
Kelvin Shen <kelvinshen@outlook.com>
2015-10-25 15:23:34 +1300

    Branching

--- FSharpPlay/Library1/Library1.fs
+++ FSharpPlay/Library1/Library1.fs
@@ -6,7 +6,13 @@ module JumpStart
 let x = 42
 let hi = "hello"
 
+let Greeting name = 
+    if System.String.IsNullOrWhiteSpace(name) then
+        "whoever you are"
+    else
+        name
+
 let SayHiTo me =
-    printfn "Hi, %s" me
+    printfn "Hi, %s" (Greeting me)
 
 let Square x = x * x
