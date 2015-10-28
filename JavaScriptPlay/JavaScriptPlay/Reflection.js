930d49b0ee93c2ed7bfe86cc5bcfa50d04cf9b01
Kelvin Shen <kelvinshen@outlook.com>
2015-10-28 15:45:06 +1300

    js reflection

---
+++ JavaScriptPlay/JavaScriptPlay/Reflection.js
@@ -0,0 +1,26 @@
+﻿var k$ = k$ || {
+    Customer: function (n, e) {
+        this.name = n;
+        this.email = e;
+    }
+};
+
+(function Main() {
+    var customer = new k$.Customer("Jeffery", "jeffery.white@test.com");
+
+    // get perperty values of an object
+    for (var prop in customer) {
+        console.log(customer[prop]);
+    }
+
+    // get property names of an object
+    for (var prop in customer) {
+        console.log(prop);
+    }
+
+    // get property names of an object
+    var keys = Object.keys(customer);
+    for (var i = 0; i < keys.length; i++) {
+        console.log(keys[i]);
+    }
+})();
