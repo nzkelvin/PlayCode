c7e3a97fba1c2230e4b81b42e95137467590ce49
Kelvin Shen <kelvinshen@outlook.com>
2015-11-05 06:00:30 +1300

    MVC Conference Demo

---
+++ MvcPlay/Conf.DataModel.Sql/ConfDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conf.DataModel.Sql
+{
+    /// <summary>
+    /// Model from https://msignite.nz/sessions
+    /// </summary>
+    public class ConfDataContext: DbContext
+    {
+        public DbSet<Conf.Model.Session> Sessions { get; set; }
+    }
+}
---
+++ MvcPlay/Conf.Model/Session.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conf.Model
+{
+    public class Session
+    {
+        public string Name { get; set; }
+    }
+}
