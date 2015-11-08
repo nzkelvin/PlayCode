cba535d9adaf868c327d262d4318bb7e26badb65
Kelvin Shen <kelvinshen@outlook.com>
2015-11-08 13:43:09 +1300

    F# array

---
+++ FSharpPlay/Library1/Module3.fs
@@ -0,0 +1,34 @@
+﻿#if INTERACTIVE
+#else
+module Module3
+#endif
+
+let arr = [|1; 2; 3|]
+
+let fruits = 
+    [|
+        "apple"
+        "orange"
+        "pear"
+    |]
+
+let numbers = [|0..4..12|]
+
+let squares = [| for i in 1..99 do yield i*i|]
+
+let RandomFruits count = 
+    let r = System.Random()
+    let fruits = [|"apple"; "orange"; "pear"|]
+    [|
+        for i in 1..count do
+            let index = r.Next(3)
+            yield fruits.[index]
+    |]
+
+let RandomFruits2 count = 
+    let r = System.Random()
+    let fruits = [|"apple"; "orange"; "pear"|]
+    Array.init count (fun i ->
+        let index = r.Next(3)
+        fruits.[index]
+    )
