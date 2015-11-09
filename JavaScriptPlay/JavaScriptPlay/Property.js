ca60760b269128796fd65828460cc8962dc3b199
Kelvin Shen <kelvinshen@outlook.com>
2015-11-10 08:23:10 +1300

    js define a property

---
+++ JavaScriptPlay/JavaScriptPlay/Property.js
@@ -0,0 +1,17 @@
+﻿"use strict"
+
+// Of course it will work with inheritance.
+var k$ = k$ || {
+    Fruit: function (name) {
+        var _name = name;
+
+        Object.defineProperty(this, "name", {
+            get: function () { return _name; }
+        })
+    }
+};
+
+(function Main() {
+    var fruit = new k$.Fruit("apple");
+    console.log(fruit.name);
+})();
---
+++ JavaScriptPlay/JavaScriptPlay/Template.js
@@ -0,0 +1,11 @@
+﻿"use strict"
+
+var k$ = k$ || {
+    MyClass: function (name) {
+
+    }
+};
+
+(function Main() {
+
+})()
