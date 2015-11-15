942575179448eec52fddcde5c3e2cf635cdf86e2
Kelvin Shen <kelvinshen@outlook.com>
2015-11-15 20:41:54 +1300

    Crm.Console

---
+++ Crm.Console/Crm.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using Crm.CrmCommon.Entities;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Client.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crm.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var xrm = new DataContext(new OrganizationService("Xrm"));
+
+            var request = new WhoAmIRequest();
+            var response = xrm.Execute(request);
+
+            System.Console.WriteLine(response.Results.FirstOrDefault());
+
+            System.Console.ReadKey();
+            
+        }
+    }
+}
