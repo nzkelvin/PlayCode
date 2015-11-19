12893641aca5243b356b73ecae920a4302720543
Kelvin Shen <kelvinshen@outlook.com>
2015-11-19 18:00:16 +1300

    EF play

---
+++ EntityFrameworkPlay/VocabBook.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+using VocabBook.DataModel.Sql;
+
+namespace VocabBook.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new VocabBookContext("VocabBookContextConnection");
+            var c = (from e in ctx.DictionaryEntries
+                     select e).Count();
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
