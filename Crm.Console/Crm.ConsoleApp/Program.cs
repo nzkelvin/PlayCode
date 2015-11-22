62207a03480835d46a2ce21b612534d60ae65b4a
Kelvin Shen <kelvinshen@outlook.com>
2015-11-23 09:33:16 +1300

    contact query

--- Crm.Console/Crm.ConsoleApp/Program.cs
+++ Crm.Console/Crm.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Crm.CrmCommon.Entities;
 using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Client.Services;
+using Microsoft.Xrm.Sdk;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +12,32 @@ namespace Crm.ConsoleApp
 {
     class Program
     {
+        private const string _connStrName = "Xrm";
+
         static void Main(string[] args)
         {
-            var xrm = new DataContext(new OrganizationService("Xrm"));
+            //var xrm = new DataContext(new OrganizationService(_connStrName));
 
-            var request = new WhoAmIRequest();
-            var response = xrm.Execute(request);
+            //var request = new WhoAmIRequest();
+            //var response = xrm.Execute(request);
 
-            Console.WriteLine(response.Results.FirstOrDefault());
+            //Console.WriteLine(response.Results.FirstOrDefault());
+            TestQuery();
 
             Console.ReadKey();
+        }
+
+        public static void TestQuery()
+        {
+            using (var ctx = new DataContext(new OrganizationService(_connStrName)))
+            {
+                var contacts = ctx.ContactSet.Select(c => c).Take(5);
+
+                foreach (var c in contacts)
+                {
+                    Console.WriteLine(c.FirstName);
+                }
+            }
         }
     }
 }
