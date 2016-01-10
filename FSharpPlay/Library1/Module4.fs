39dfe95b86969cf5d3ba74085aceededb5ac4bbf
Kelvin Shen <kelvinshen@outlook.com>
2016-01-11 11:10:26 +1300

    F# module 4

---
+++ FSharpPlay/Library1/Module4.fs
@@ -0,0 +1,19 @@
+﻿#if INTERACTIVE
+#else
+module Module4
+#endif
+
+type Person = 
+    {
+        FirstName: string
+        LastName: string
+    }
+
+let person = { FirstName = "Kit"; LastName = "Shen" }
+printfn "%s, %s" person.FirstName person.LastName
+
+type Company = 
+    {
+        Name: string
+        SalesTaxNumber: int option
+    }
