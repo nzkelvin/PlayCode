c8d6ba0adf96ccbe57abf4890a99081b4c3bf592
Kelvin Shen <kelvinshen@outlook.com>
2016-01-24 11:26:32 +1300

    underscore.js

--- NodeJsPlay/NodeJsPlay.ConsoleApp1/app.js
+++ NodeJsPlay/NodeJsPlay.ConsoleApp1/app.js
@@ -8,3 +8,6 @@ var msgs = require("./msgs.js");
 var msg = new msgs();
 console.log(msg.message1);
 console.log(msg.firstObj);
+
+var underscore = require("underscore");
+console.log(underscore.contains([1, 2, 3] , 3));
