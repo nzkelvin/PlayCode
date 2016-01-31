45165bb7c13352c14a9d099a438ecbe0fb3fbf2d
Kelvin Shen <kelvinshen@outlook.com>
2016-02-01 07:04:57 +1300

    fsharp console pause

--- FSharpPlay/FSharpPlay.Console/Program.fs
+++ FSharpPlay/FSharpPlay.Console/Program.fs
@@ -1,7 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+//todo:
+//how to import modules
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    System.Console.ReadKey() |> ignore // ignore to avoid a compile error but looks like no longer needed.
     0 // return an integer exit code
