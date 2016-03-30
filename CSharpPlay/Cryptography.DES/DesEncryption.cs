8c92e6108f19025d10f45efe0ed03d6f3def1aaf
Kelvin Shen <kelvinshen@outlook.com>
2016-03-31 12:56:07 +1300

    DES encryption

---
+++ CSharpPlay/Cryptography.DES/DesEncryption.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography.DES
+{
+    public class DesEncryption
+    {
+        public byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+
+        public byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            using (var des = new DESCryptoServiceProvider())
+            {
+                des.Mode = CipherMode.CBC;
+                des.Padding = PaddingMode.PKCS7;
+
+                des.Key = key;
+                des.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, des.CreateEncryptor(), CryptoStreamMode.Write);
+
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ CSharpPlay/Cryptography.DES/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography.DES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataToEncrypt = Encoding.UTF8.GetBytes("MyPassword");
+            var des = new DesEncryption();
+            var key = des.GenerateRandomNumber(8);
+            var iv = des.GenerateRandomNumber(8);
+            var encryptedData = des.Encrypt(dataToEncrypt, key, iv);
+
+            Console.WriteLine(Encoding.UTF8.GetString(dataToEncrypt));
+            Console.WriteLine(Encoding.UTF8.GetString(encryptedData));
+            Console.ReadLine();
+        }
+    }
+}
