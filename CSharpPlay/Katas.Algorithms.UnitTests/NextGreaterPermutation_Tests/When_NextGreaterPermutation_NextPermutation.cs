1332a2a2253125c88dadf3b03597d68cdc078a0b
Kelvin Shen <kelvinshen@outlook.com>
2016-09-24 23:37:40 +1200

    Algorithm: Next Greater Permutation

---
+++ CSharpPlay/Katas.Algorithms.UnitTests/NextGreaterPermutation_Tests/When_NextGreaterPermutation_NextPermutation.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Katas.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.Algorithms.NextGreaterPermutation_Tests
+{
+    [TestClass()]
+    public class When_NextGreaterPermutation_NextPermutation
+    {
+        //[TestMethod()]
+        public void Given_5264_Then_5462()
+        {
+            //Arrange
+            var permutationGenerator = new NextGreaterPermutation();
+            var actual = new int[] { 7, 2, 6, 5, 4 };
+            var expected = new int[] { 7, 4, 6, 5, 2 };
+
+            //Action
+            permutationGenerator.NextPermutation(actual);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Given_132_Then_213() // All digits swapped
+        {
+            //Arrange
+            var permutationGenerator = new NextGreaterPermutation();
+            var actual = new int[] { 1, 3, 2 };
+            var expected = new int[] { 2, 1, 3 };
+
+            //Action
+            permutationGenerator.NextPermutation(actual);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Given_123_Then_132() // partial digits swapped
+        {
+            //Arrange
+            var permutationGenerator = new NextGreaterPermutation();
+            var actual = new int[] { 1, 2, 3 };
+            var expected = new int[] { 1, 3, 2 };
+
+            //Action
+            permutationGenerator.NextPermutation(actual);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod()]
+        public void Given_NoNextGreaterPermutation321_Then_ReturnMinimumPermutation123()
+        {
+            //Arrange
+            var permutationGenerator = new NextGreaterPermutation();
+            var actual = new int[] { 3, 2, 1 };
+            var expected = new int[] { 1, 2, 3 };
+
+            //Action
+            permutationGenerator.NextPermutation(actual);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Given_231_Then_312()
+        {
+            //Arrange
+            var permutationGenerator = new NextGreaterPermutation();
+            var actual = new int[] { 2, 3, 1 };
+            var expected = new int[] { 3, 1, 2 };
+
+            //Action
+            permutationGenerator.NextPermutation(actual);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Test 2 same value digits
+        /// </summary>
+        [TestMethod()]
+        public void Given_151_Then_511()
+        {
+            //Arrange
+            var permutationGenerator = new NextGreaterPermutation();
+            var actual = new int[] { 1, 5, 1 };
+            var expected = new int[] { 5, 1, 1 };
+
+            //Action
+            permutationGenerator.NextPermutation(actual);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CSharpPlay/Katas.Algorithms/NextGreaterPermutation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.Algorithms
+{
+    public class NextGreaterPermutation
+    {
+        public void NextPermutation(int[] nums)
+        {
+            if (nums.Length <= 1)
+                return;
+
+            // find a[i-1] < a[i]
+            var i = nums.Length - 1;
+
+            while (i > 0 && nums[i - 1] >= nums[i])  i--;
+
+            //if (i == 0 && nums[i] >= nums[i + 1]) reverse(nums);
+            var j = i;
+            if (i > 0 && nums[i - 1] < nums[i])
+            {
+                // find j where a[j] just bigger than a[i - 1]
+                //var j = i;
+
+                while (j < nums.Length && nums[i - 1] < nums[j]) j++;
+
+                swap(nums, i - 1, j - 1);
+            }
+            //else i--;
+
+            reverse(nums, i, nums.Length - 1);
+        }
+
+        private void swap(int[] nums, int leftIdx, int rightIdx)
+        {
+            var temp = nums[leftIdx];
+            nums[leftIdx] = nums[rightIdx];
+            nums[rightIdx] = temp;
+        }
+
+        private void reverse(int[] nums, int leftIdx, int rightIdx)
+        {
+            //var leftIdx = 0;
+            //var rightIdx = nums.Length - 1;
+
+            while (leftIdx < rightIdx)
+            {
+                swap(nums, leftIdx, rightIdx);
+                leftIdx++;
+                rightIdx--;
+            }
+        }
+    }
+}
