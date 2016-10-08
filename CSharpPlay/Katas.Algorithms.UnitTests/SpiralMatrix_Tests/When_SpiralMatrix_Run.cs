7c576027eaeb30a33a014cc930f61d3c50da1d66
Kelvin Shen <kelvinshen@outlook.com>
2016-10-08 18:06:57 +1300

    Algorithm: Spiral Matrix.

---
+++ CSharpPlay/Katas.Algorithms.UnitTests/SpiralMatrix_Tests/When_SpiralMatrix_Run.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katas.Algorithms.UnitTests.SpiralMatrix_Tests
+{
+    [TestClass]
+    public class When_SpiralMatrix_Run
+    {
+        [TestMethod]
+        public void Given_3By3Matrix_Then_ProduceASpiralArray()
+        {
+            // Arrange
+            int[][] input = new []{
+                                new []{ 1, 2, 3 },
+                                new []{ 4, 5, 6 },
+                                new []{ 7, 8, 9 }
+                              };
+
+            var expected = new[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+
+            // Act
+            var spiralGenerator = new SpiralMatrix();
+            var actual = spiralGenerator.Run(input);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Given_NullMatrix_Then_ProduceNull()
+        {
+            // Arrange
+            int[][] input = null;
+
+            int[][] expected = null;
+
+            // Act
+            var spiralGenerator = new SpiralMatrix();
+            var actual = spiralGenerator.Run(input);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+    }
+}
--- CSharpPlay/Katas.Algorithms/SpiralMatrix.cs
+++ CSharpPlay/Katas.Algorithms/SpiralMatrix.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace Katas.Algorithms
 {
-    class SpiralMatrix
+    public class SpiralMatrix
     {
+        /// <summary>
+        /// What I learnt:
+        /// * Switch case 
+        /// * The way of thinking
+        /// 
+        /// Todo:
+        /// * Abstract with functional programming
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public int[] Run(int[][] input)
+        {
+            if (input == null || input.Length < 1 || input[0] == null || input[0].Length < 1)
+                return null;
+
+            var runCounter = 1;
+            var side = 1;
+            var topWall = 0;
+            var rightWall = input[0].Length - 1;
+            var bottomWall = input.Length - 1;
+            var leftWall = 0;
+            var spiral = new List<int>();
+
+            while (topWall <= bottomWall && leftWall <= rightWall)
+            {
+                switch (side)
+                {
+                    case 1: //Top
+                        for (var i = leftWall; i <= rightWall; i++) spiral.Add(input[topWall][i]);
+                        topWall++;
+                        break;
+                    case 2: //Right
+                        for (var i = topWall; i <= bottomWall; i++) spiral.Add(input[i][rightWall]);
+                        rightWall--;
+                        break;
+                    case 3: //Bottom
+                        for (var i = rightWall; i >= leftWall; i--) spiral.Add(input[bottomWall][i]);
+                        bottomWall--;
+                        break;
+                    case 4: //Left
+                        for (var i = bottomWall; i >= topWall; i--) spiral.Add(input[i][leftWall]);
+                        leftWall++;
+                        break;
+                }
+
+                runCounter++;
+                side = runCounter % 4;
+            }
+
+            return spiral.ToArray();
+        }
     }
 }
