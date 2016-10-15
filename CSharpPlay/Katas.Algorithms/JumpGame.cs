99c60e52b0837c524d16571a9186def2f201a787
Kelvin Shen <kelvinshen@outlook.com>
2016-10-15 22:07:24 +1300

    more comment

--- CSharpPlay/Katas.Algorithms/JumpGame.cs
+++ CSharpPlay/Katas.Algorithms/JumpGame.cs
@@ -9,7 +9,7 @@ namespace Katas.Algorithms
     /// <summary>
     /// https://leetcode.com/articles/jump-game/
     /// todo: I will try approach 2, 3 and 4
-    /// key words: Top down, bottom up, dynamics programming (recursive is an example?)
+    /// key words: Top down, bottom up, back tracking, dynamics programming (recursive is an example?)
     /// </summary>
     public class JumpGame
     {
